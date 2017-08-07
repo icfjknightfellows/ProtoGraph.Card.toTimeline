96853e8b1bbc1c5edbc85ae4b927d90822e31e39
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-08-07 13:55:00 +0530

    added gzipping

--- webpack.config.card.js
+++ webpack.config.card.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack');
+const webpack = require('webpack'),
+  CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: './main.js',
@@ -12,6 +13,16 @@ module.exports = {
         NODE_ENV: JSON.stringify('production')
       }
     }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({ 
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
   ],
   node: {
     net: 'empty',
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 const webpack = require('webpack');
 const card = require('./webpack.config.card.js');
 const edit_card = require('./webpack.config.edit_card.js');
+const express = require('express');
+const app = express();
+
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+});
+
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join( __dirname, 'index.html'));
+// });
 
 module.exports = [
   card,
